62b8f260b3c08d96a4e2f084d598a1666779d498
Johannes Künsebeck <jkuensebeck@taz.de>
2020-07-27 14:31:16 +0200

    Refactored lazy loading to use concurrent.futures

--- ocrd_browser/icon_store.py
+++ ocrd_browser/icon_store.py
@@ -1,119 +1,84 @@
 import gi
-gi.require_version('Gtk','3.0')
-from gi.repository import GObject, Gtk, GdkPixbuf
 
-import threading
-from ocrd_utils import pushd_popd
-from ocrd_browser.image_util import pil_to_pixbuf
-from ocrd_browser.model import Document
+gi.require_version('Gtk', '3.0')
+from gi.repository import GObject, Gtk, GLib
+
+import concurrent.futures
 
 
-class LazyLoadingPixbufListStore(Gtk.ListStore):
-    def __init__(self, types: tuple, pixbuf_column: int, pixbuf_callback,
-                 loading_image_pixbuf: GdkPixbuf.Pixbuf = None):
-        super().__init__(*types)
-        self.pixbuf_column = pixbuf_column
-        self.pixbuf_callback = pixbuf_callback
-        self.loading = False
+class LazyLoadingListStore(Gtk.ListStore):
+    init_row = GObject.Property()
+    load_row = GObject.Property()
+    hash_row = GObject.Property()
 
-        if loading_image_pixbuf:
-            self.loading_image_pixbuf = loading_image_pixbuf
-        else:
-            theme = Gtk.IconTheme.get_default()
-            self.loading_image_pixbuf = theme.load_icon_for_scale('image-loading', Gtk.IconSize.LARGE_TOOLBAR, 40, 0)
+    def __init__(self, *column_types, init_row, load_row, hash_row):
+        column_types = list(column_types)
+        column_types.append(str)
+        super().__init__(*column_types)
+        self.init_row = init_row
+        self.load_row = load_row
+        self.hash_row = hash_row
+        self.futures = None
+        self.row_inserted_handler = self.connect('row-inserted', self._on_row_inserted)
+        self.row_changed_handler = self.connect('row-changed', self._on_row_changed)
 
-        self.connect('row-inserted', self.on_row_inserted)
-        self.connect('row-changed', self.on_row_changed)
+    def start_loading(self):
+        self.futures = {}
+        self.submit_all()
+        GLib.idle_add(self._collect_workers().__next__, priority=GLib.PRIORITY_DEFAULT_IDLE)
 
-    def on_row_inserted(self, *_):
-        self.reload()
-
-    def on_row_changed(self, store, it, path):
-        pass
-        # print('change', store, it)
-        # TODO: This will lead to recursion,
-        # instead do something clever in reload like
-        # storing a hash (or filename + last modified or ...) in one column and compare with current
-        # store[it][self.pixbuf_column] = None
-        # self.reload()
-
-    def reload(self):
+    def submit_all(self):
+        pool = concurrent.futures.ThreadPoolExecutor()
         for row in self:
-            if row[self.pixbuf_column] is None:
-                row[self.pixbuf_column] = self.loading_image_pixbuf
+            self._submit_future(row, pool)
+        pool.shutdown(wait=False)
+        return True
 
-        if not self.loading:
-            thread = threading.Thread(target=self.load_images)
-            thread.start()
+    def _submit_future(self, row, pool=None):
+        if self.futures is None:
+            return
+        own_pool = pool is None
+        if own_pool:
+            pool = concurrent.futures.ThreadPoolExecutor()
 
-    def load_images(self):
-        self.loading = True
-        did_something = True
-        while did_something:
-            did_something = False
-            for row in self:
-                pixbuf = row[self.pixbuf_column]
-                if pixbuf is None or pixbuf is self.loading_image_pixbuf:
-                    did_something = self.pixbuf_callback(row)
+        row_hash = self.hash_row(row)
 
-        self.loading = False
+        if row[-1] != row_hash and row not in self.futures.values():
+            future = pool.submit(self.load_row, row[:])
+            self.futures[future] = row
 
+        if own_pool:
+            pool.shutdown(wait=False)
 
-@Gtk.Template(resource_path="/org/readmachine/ocrd-browser/ui/page-list.ui")
-class PagePreviewList(Gtk.IconView):
-    __gtype_name__ = "PagePreviewList"
+    def _do_insert(self, position, row):
+        if row is not None:
+            row = tuple(row) + (None,)
+        super()._do_insert(position, row)
 
-    def __init__(self, document: Document, **kwargs):
-        super().__init__(**kwargs)
-        self.document = document
-        self.current: Gtk.TreeIter = None
-        self.set_text_column(0)
-        self.set_tooltip_column(1)
-        self.set_pixbuf_column(2)
-        self.set_item_width(50)
-        text_renderer: Gtk.CellRendererText = \
-        [cell for cell in self.get_cells() if isinstance(cell, Gtk.CellRendererText)][0]
-        text_renderer.ellipsize = 'middle'
-        self.model = self._setup_model()
-        self.set_model(self.model)
-        GLib.idle_add(self.model.reload)
-        GLib.idle_add(self.goto_index, 0)
+    def _on_row_inserted(self, list_store: Gtk.ListStore, _path: Gtk.TreePath, it: Gtk.TreeIter):
+        row = list_store[it]
+        with self.handler_block(self.row_changed_handler):
+            self.init_row(row)
+        self._submit_future(row)
 
-    def goto_index(self, index):
-        index = index if index >= 0 else len(self.model) + index
-        if 0 <= index < len(self.model):
-            self.goto_path(Gtk.TreePath(index))
+    def _on_row_changed(self, list_store: Gtk.ListStore, _path: Gtk.TreePath, it: Gtk.TreeIter):
+        row = list_store[it]
+        self._submit_future(row)
 
-    def skip(self, pos):
-        iterate = self.model.iter_previous if pos < 0 else self.model.iter_next
-        n = iterate(self.current)
-        self.goto_path(self.model.get_path(n))
-
-    def goto_path(self, path):
-        self.unselect_all()
-        self.select_path(path)
-        self.set_cursor(path, None, False)
-        self.emit('activate_cursor_item')
-
-    def _setup_model(self):
-        model = LazyLoadingPixbufListStore((str, str, GdkPixbuf.Pixbuf), 2, self.pixbuf_loader)
-        for page_id in self.document.page_ids:
-            model.append([page_id, 'Loading', None])
-        return model
-
-    def pixbuf_loader(self, row: Gtk.TreeModelRow):
-        page_id = row[0]
-        with pushd_popd(self.document.workspace.directory):
-            page = self.document.page_for_id(page_id)
-        thumb = page.image.copy()
-        thumb.thumbnail((100, 500))
-        row[1] = '{0} ({1}x{2})'.format(page.page.imageFilename, page.page.imageWidth, page.page.imageHeight)
-        row[2] = pil_to_pixbuf(thumb)
-
-    def do_item_activated(self, path: Gtk.TreePath):
-        self.current = self.model.get_iter(path)
-        self.emit('page_selected', self.model[path][0])
-
-    @GObject.Signal()
-    def page_selected(self, page_id: str):
-        pass
+    def _collect_workers(self):
+        for future in concurrent.futures.as_completed(self.futures):
+            row = self.futures.pop(future)
+            try:
+                new_row_data = future.result()
+            except Exception as exc:
+                print('{} generated an exception: {}'.format(row[0], exc))
+            else:
+                with self.handler_block(self.row_changed_handler):
+                    for i, (old, new) in enumerate(zip(row[:], new_row_data)):
+                        if old != new:
+                            row[i] = new_row_data[i]
+                    row[-1] = self.hash_row(row)
+                yield True
+        # Futures are finished for now, check back every 50ms if there is something new
+        GLib.timeout_add(50, self._collect_workers().__next__, priority=GLib.PRIORITY_DEFAULT_IDLE)
+        yield False
---
+++ tests/test_icon_store.py
@@ -0,0 +1,71 @@
+import gi
+
+gi.require_version('Gtk', '3.0')
+from gi.repository import Gtk, GdkPixbuf, GObject, GLib
+
+import unittest
+from ocrd_browser.icon_store import LazyLoadingListStore
+
+
+class LazyLoadingListStoreTestCase(unittest.TestCase):
+
+    def setUp(self):
+        def call_instantly(fun, *args, **kwargs):
+            return fun(*args, **kwargs)
+
+        self.types = (str, int, GdkPixbuf.Pixbuf)
+        self.list_store = Gtk.ListStore(*self.types)
+        self.lazy_list_store = LazyLoadingListStore(*self.types, init_row=self.logging_init, load_row=self.logging_load,
+                                                    hash_row=self.hash_row)
+        self.init_log = []
+        self.backup_idle_add = GLib.idle_add
+        GLib.idle_add = call_instantly
+
+    def tearDown(self) -> None:
+        GLib.idle_add = self.backup_idle_add
+
+    def logging_init(self, row: Gtk.TreeModelRow):
+        self.init_log.append(row[:])
+
+    def logging_load(self, row: Gtk.TreeModelRow):
+        pass
+        # self.init_log.append(row[:])
+
+    def hash_row(self, row: Gtk.TreeModelRow):
+        return row[0]
+
+    def test_constructor_propagates_types(self):
+        for i in range(0, self.list_store.get_n_columns()):
+            self.assertEqual(self.list_store.get_column_type(i), self.lazy_list_store.get_column_type(i))
+
+    def test_constructor_adds_hash_column(self):
+        list_width = self.list_store.get_n_columns()
+        lazy_list_width = self.lazy_list_store.get_n_columns()
+        self.assertEqual(list_width + 1, lazy_list_width)
+        hash_column_type = self.lazy_list_store.get_column_type(lazy_list_width - 1)
+        str_as_gtype = GObject.GType.from_name('gchararray')
+        self.assertEqual(str_as_gtype, hash_column_type)
+
+    def test_append_adds_hash_column(self):
+        row_data = ('a', 5, None)
+        self.lazy_list_store.append(row_data)
+        actual = self.lazy_list_store[-1][:]
+        self.assertSequenceEqual(row_data + (None,), actual)
+
+    def test_append_calls_init_row(self):
+        row_data = ('a', 5, None)
+        self.lazy_list_store.append(row_data)
+        self.assertEqual(1, len(self.init_log))
+        self.assertSequenceEqual(row_data, self.init_log[0][:len(row_data)])
+
+    def test_init_row_can_modify_model(self):
+        def change_row(row: Gtk.TreeModelRow):
+            row[1] = 15
+
+        self.lazy_list_store.init_row = change_row
+        self.lazy_list_store.append(('a', 5, None))
+        self.assertEqual(self.lazy_list_store[-1][1], 15)
+
+
+if __name__ == '__main__':
+    unittest.main()
