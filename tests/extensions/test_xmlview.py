41c71dbe59f66f650c47adcfe4503df11e895715
Johannes Künsebeck <jkuensebeck@taz.de>
2020-07-29 19:37:33 +0200

    mock document

--- tests/extensions/test_xmlview.py
+++ tests/extensions/test_xmlview.py
@@ -1,12 +1,12 @@
 import unittest
+from unittest.mock import MagicMock
 from ocrd_browser.extensions.xmlview import ViewXml
 from ocrd_browser.model import Document
-
 
 class XmlViewTestCase(unittest.TestCase):
 
     def setUp(self):
-        self.vx = ViewXml(document=Document.create())
+        self.vx = ViewXml(document=MagicMock(spec=Document))
 
     def test_can_construct(self):
         self.assertIsNotNone(self.vx)
