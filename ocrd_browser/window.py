98eaa6afa1a45cd503127be0cc0335c094959e7a
Johannes Künsebeck <jkuensebeck@taz.de>
2020-08-03 21:57:22 +0200

    Basic structure for context menus / page deletion

--- ocrd_browser/window.py
+++ ocrd_browser/window.py
@@ -1,8 +1,10 @@
-from gi.repository import Gtk, GdkPixbuf, Gio, GObject, GLib, Pango
+from typing import List
+
+from gi.repository import Gtk, GdkPixbuf, Gio, GObject, GLib, Pango, Gdk
 
 from ocrd_browser import __version__
 from ocrd_browser.extensions.physical_import import ViewScan
-from ocrd_browser.model import Document
+from ocrd_browser.model import Document, DEFAULT_FILE_GROUP
 from ocrd_browser.view import ViewRegistry, ViewImages, View
 from ocrd_browser.icon_store import LazyLoadingListStore
 from ocrd_browser.image_util import cv_scale, cv_to_pixbuf
@@ -30,7 +32,7 @@ class MainWindow(Gtk.ApplicationWindow):
         self.set_icon(GdkPixbuf.Pixbuf.new_from_resource("/org/readmachine/ocrd-browser/icons/icon.png"))
         self.views = []
         self.current_page_id = None
-        self.document = Document.create()
+        self.document = Document.create(self)
 
         vstring = GLib.Variant("s", "")
 
@@ -40,6 +42,8 @@ class MainWindow(Gtk.ApplicationWindow):
         self.actions.create('go_back')
         self.actions.create('go_forward')
         self.actions.create('goto_last')
+        self.actions.create('page_remove')
+        self.actions.create('page_properties')
         self.actions.create('close_view', param_type=vstring)
         self.actions.create('create_view', param_type=vstring)
 
@@ -56,6 +60,12 @@ class MainWindow(Gtk.ApplicationWindow):
 
         self.update_ui()
 
+    def on_page_remove(self, _a: Gio.SimpleAction, _p):
+        for page_id in self.page_list.get_selected_ids():
+            self.document.delete_page(page_id)
+
+    def on_page_properties(self, _a: Gio.SimpleAction, _p):
+        print(self.page_list.get_selected_items())
 
 
     @Gtk.Template.Callback()
@@ -74,7 +84,7 @@ class MainWindow(Gtk.ApplicationWindow):
         GLib.timeout_add(50, self._open, uri)
 
     def _open(self, uri):
-        self.document = Document.load(uri)
+        self.document = Document.load(self,uri)
         self.page_list.set_document(self.document)
 
         title = self.document.workspace.mets.unique_identifier if self.document.workspace.mets.unique_identifier else '<unnamed>'
@@ -112,6 +122,16 @@ class MainWindow(Gtk.ApplicationWindow):
         self.current_page_label.set_text('{}/{}'.format(index + 1, len(self.document.page_ids)))
         self.update_ui()
         pass
+
+    @GObject.Signal(arg_types=(object,))
+    def document_changed(self, page_ids: List):
+        print(page_ids)
+        self.page_list.document_changed(page_ids)
+
+    @GObject.Signal()
+    def document_saved(self):
+        print('saved')
+
 
     def update_ui(self):
         can_go_back = False
@@ -197,24 +217,75 @@ class PagePreviewList(Gtk.IconView):
         self.loading_image_pixbuf: GdkPixbuf.Pixbuf = None
         self.file_lookup: dict = None
         self.setup_ui()
+        self.setup_context_menu()
         self.set_document(document)
+
+    def setup_context_menu(self):
+        menu = Gio.Menu()
+        action_menu = Gio.Menu()
+        action_menu.append('Remove', 'win.page_remove')
+        menu.append_section(None, action_menu)
+        prop_menu = Gio.Menu()
+        prop_menu.append('Properties', 'win.page_properties')
+        menu.append_section(None, prop_menu)
+        self.cmenu: Gtk.Menu = Gtk.Menu()
+        self.cmenu.bind_model(menu, None, True)
+        self.cmenu.attach_to_widget(self)
+        self.cmenu.show_all()
 
     def set_document(self, document):
         self.document = document
         self.setup_model()
 
+    def document_changed(self, page_ids: List):
+        to_delete = []
+        for n,row in enumerate(self.model):
+            if row[0] in page_ids:
+                page_ids.remove(row[0])
+                files = self.document.workspace.mets.find_files(fileGrp=DEFAULT_FILE_GROUP, pageId=row[0])
+                if files:
+                    file_name = str(self.document.path(files[0].local_filename))
+                    row[2] = file_name
+                else:
+                    to_delete.append(n)
+
+        for delete in reversed(to_delete):
+            self.model.remove(self.model.get_iter(Gtk.TreePath(delete)))
+
+        for page_id in page_ids:
+            file = self.document.workspace.mets.find_files(fileGrp=DEFAULT_FILE_GROUP, pageId=page_id)[0]
+            file_name = str(self.document.path(file.local_filename))
+            self.model.append((page_id, '', file_name, None))
+
+
+
+
     def setup_ui(self):
         self.loading_image_pixbuf = GdkPixbuf.Pixbuf.new_from_resource('/org/readmachine/ocrd-browser/icons/loading.png')
-
         self.set_text_column(0)
         self.set_tooltip_column(1)
         self.set_pixbuf_column(3)
         text_renderer: Gtk.CellRendererText = \
             [cell for cell in self.get_cells() if isinstance(cell, Gtk.CellRendererText)][0]
         text_renderer.props.ellipsize = Pango.EllipsizeMode.MIDDLE
+        self.connect('button-release-event', self.button_pressed)
+
+    def button_pressed(self, _sender, event: Gdk.EventButton):
+        if event.get_button()[1] == 3:
+            path, renderer = self.get_item_at_pos(*event.get_coords())
+            row = self.model[path]
+            self.emit('on_context_menu', event, row, path, renderer)
+
+    @GObject.Signal(arg_types=(object, object, object, object))
+    def on_context_menu(self, event: Gdk.EventButton, row: Gtk.TreeModelRow, path: Gtk.TreePath, renderer: Gtk.CellRenderer):
+        if len(self.get_selected_items()) <= 1:
+            self.set_cursor(path, None, False)
+            self.emit('select_cursor_item')
+        self.cmenu.popup_at_pointer(event)
+
 
     def setup_model(self):
-        self.file_lookup = self.get_image_paths()
+        self.file_lookup = self.get_image_paths(self.document)
         self.model = LazyLoadingListStore(str, str, str, GdkPixbuf.Pixbuf,
                                           init_row=self.init_row, load_row=self.load_row, hash_row=self.hash_row)
         for page_id in self.document.page_ids:
@@ -240,11 +311,15 @@ class PagePreviewList(Gtk.IconView):
         mtime = os.path.getmtime(file)
         return '{}:{}'.format(file, mtime)
 
-    def get_image_paths(self, file_group='OCR-D-IMG'):
-        images = self.document.workspace.mets.find_files(fileGrp=file_group)
-        page_ids = self.document.workspace.mets.get_physical_pages(for_fileIds=[image.ID for image in images])
-        file_paths = [self.document.path(image.url) for image in images]
+    @staticmethod
+    def get_image_paths(document: Document, file_group='OCR-D-IMG'):
+        images = document.workspace.mets.find_files(fileGrp=file_group)
+        page_ids = document.workspace.mets.get_physical_pages(for_fileIds=[image.ID for image in images])
+        file_paths = [document.path(image.url) for image in images]
         return dict(zip(page_ids, file_paths))
+
+    def get_selected_ids(self):
+        return [self.model[path][0] for path in self.get_selected_items()]
 
     def goto_index(self, index):
         index = index if index >= 0 else len(self.model) + index
@@ -252,21 +327,20 @@ class PagePreviewList(Gtk.IconView):
             self.goto_path(Gtk.TreePath(index))
 
     def skip(self, pos):
+        if not self.current:
+            self.current = self.model.get_iter(Gtk.TreePath(0))
         iterate = self.model.iter_previous if pos < 0 else self.model.iter_next
         n = iterate(self.current)
         self.goto_path(self.model.get_path(n))
 
     def goto_path(self, path):
-        self.unselect_all()
-        self.select_path(path)
         self.set_cursor(path, None, False)
-        self.set_cursor(path, None, False)
+        self.emit('select_cursor_item')
         self.emit('activate_cursor_item')
+        self.grab_focus()
 
     def do_item_activated(self, path: Gtk.TreePath):
         self.current = self.model.get_iter(path)
-        # Trigger on_row_changed
-        self.model[self.current][0] = self.model[self.current][0]
         self.emit('page_selected', self.model[path][0])
 
     @GObject.Signal()
