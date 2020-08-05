8f019e5533c7eddb809939ec053f3763893c5ab1
Johannes Künsebeck <jkuensebeck@taz.de>
2020-08-05 16:16:15 +0200

    implemented Document.reorder

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,12 +8,19 @@ import with:
 
 in your Gtk using test file in the first line
 """
+# pylint: disable=unused-import
+from unittest import TestCase
 import gi
+from pathlib import Path
 gi.require_version('Gtk', '3.0')
 gi.require_version('GdkPixbuf', '2.0')
 try:
     gi.require_version('GtkSource', '4')
 except ValueError:
     gi.require_version('GtkSource', '3.0')
+
+ASSETS_PATH = (Path(__file__).parent/ 'assets').absolute()
+if not ASSETS_PATH.exists():
+    raise RuntimeError('Assset path {} not found, please run: make tests/assets first')
 
 
