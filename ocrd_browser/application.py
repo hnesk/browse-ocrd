96c124b4efd86b4131772fe7f2dbef22b0b3236c
Johannes Künsebeck <jkuensebeck@taz.de>
2020-08-06 16:03:48 +0200

    Type annotations for main.py and application.py

--- ocrd_browser/application.py
+++ ocrd_browser/application.py
@@ -8,13 +8,13 @@ from ocrd_browser.view import ViewRegistry
 
 
 class OcrdBrowserApplication(Gtk.Application):
-    def __init__(self):
+    def __init__(self) -> None:
         Gtk.Application.__init__(self, application_id='org.readmachine.ocrd-browser',
                                  flags=Gio.ApplicationFlags.HANDLES_OPEN)
         self.actions = ActionRegistry(for_widget=self)
         self.view_registry = ViewRegistry.create_from_entry_points()
 
-    def do_startup(self):
+    def do_startup(self) -> None:
         Gtk.Application.do_startup(self)
         self.actions.create('new')
         self.actions.create('open')
@@ -23,36 +23,36 @@ class OcrdBrowserApplication(Gtk.Application):
         for entry_point in pkg_resources.iter_entry_points('ocrd_browser_ext'):
             (entry_point.load())(self)
 
-    def do_activate(self):
+    def do_activate(self) -> None:
         win = self.get_active_window()
         if not win:
             win = MainWindow(application=self)
         win.present()
 
-    def on_about(self, _action, _param):
+    def on_about(self, _action: Gio.SimpleAction, _param: str = None) -> None:
         about_dialog = AboutDialog(application=self, transient_for=self.get_active_window(), modal=True)
         about_dialog.present()
 
-    def on_quit(self, _action, _param):
+    def on_quit(self, _action: Gio.SimpleAction, _param: str = None) -> None:
         self.quit()
 
-    def on_open(self, _action, _param):
+    def on_open(self, _action: Gio.SimpleAction, _param: str = None) -> None:
         open_dialog = OpenDialog(application=self, transient_for=self.get_active_window(), modal=True)
         response = open_dialog.run()
         if response == Gtk.ResponseType.OK:
             self.open_in_window(open_dialog.get_uri(), window=open_dialog.get_transient_for())
         open_dialog.destroy()
 
-    def on_new(self, _action, _param):
+    def on_new(self, _action: Gio.SimpleAction, _param: str = None) -> None:
         win = MainWindow(application=self)
         win.present()
 
-    def do_open(self, files: List[Gio.File], file_count: int, hint: str):
+    def do_open(self, files: List[Gio.File], file_count: int, hint: str) -> int:
         for file in files:
             self.open_in_window(file.get_uri(), window=None)
         return 0
 
-    def open_in_window(self, uri, window=None):
+    def open_in_window(self, uri: str, window: MainWindow = None) -> None:
         if not window or not window.document.empty:
             window = MainWindow(application=self)
         window.present()
--- ocrd_browser/main.py
+++ ocrd_browser/main.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 # -*- Mode: Python; coding: utf-8; indent-tabs-mode: t; c-basic-offset: 4; tab-width: 4 -*-
-import gi
 import sys
+
+import gi
 
 gi.require_version('Gtk', '3.0')
 gi.require_version('GdkPixbuf', '2.0')
@@ -12,18 +13,20 @@ except ValueError:
 
 from gi.repository import Gtk, Gio
 from pathlib import Path
+from typing import Type
+from types import TracebackType
 
 BASE_PATH = Path(__file__).absolute().parent
 resources = Gio.resource_load(str(BASE_PATH / "ui.gresource"))
 Gio.resources_register(resources)
 
 
-def install_excepthook():
+def install_excepthook() -> None:
     """ Make sure we exit when an unhandled exception occurs. """
     old_hook = sys.excepthook
 
-    def new_hook(etype, evalue, etb):
-        old_hook(etype, evalue, etb)
+    def new_hook(type_: Type[BaseException], value: BaseException, traceback: TracebackType) -> None:
+        old_hook(type_, value, traceback)
         while Gtk.main_level():
             Gtk.main_quit()
         sys.exit()
@@ -31,7 +34,7 @@ def install_excepthook():
     sys.excepthook = new_hook
 
 
-def main():
+def main() -> None:
     from ocrd_browser.application import OcrdBrowserApplication
     install_excepthook()
     app = OcrdBrowserApplication()
@@ -41,4 +44,5 @@ def main():
 if __name__ == "__main__":
     # WHY OH WHY
     sys.path.append(str(BASE_PATH.parent))
-    sys.exit(main())
+    main()
+    sys.exit()
