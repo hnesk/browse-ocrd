40d7721109cca290a4c2ea7ac46f78ec442ec559
Johannes Künsebeck <jkuensebeck@taz.de>
2020-08-06 16:03:48 +0200

    Type annotations for main.py and application.py

--- ocrd_browser/util/gtk.py
+++ ocrd_browser/util/gtk.py
@@ -2,13 +2,15 @@ from typing import Callable, Dict, Optional
 
 from gi.repository import Gio, GLib
 
+ActionCallback = Optional[Callable[[Gio.SimpleAction, str], None]]
+
 
 class ActionRegistry:
     def __init__(self, for_widget: Gio.ActionMap):
         self.for_widget = for_widget
         self.actions: Dict[str, Gio.SimpleAction] = {}
 
-    def create(self, name: str, callback: Optional[Callable[[Gio.SimpleAction, str], None]] = None,
+    def create(self, name: str, callback: ActionCallback = None,
                param_type: GLib.Variant = None) -> Gio.SimpleAction:
         callback = callback if callback else getattr(self.for_widget, 'on_' + name)
         if param_type is not None:
