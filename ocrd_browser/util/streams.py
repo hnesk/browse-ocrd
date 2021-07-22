286921daa87dcfd9d2db859f31489220061b5ae9
Johannes Künsebeck <jkuensebeck@taz.de>
2021-07-22 14:15:19 +0200

    Code cleanup for silenced streams

--- ocrd_browser/util/streams.py
+++ ocrd_browser/util/streams.py
@@ -1,5 +1,9 @@
 import os
+from types import TracebackType
+from typing import TextIO, Optional, Type
+
 import sys
+
 
 class RedirectedStdStreams:
     """
@@ -10,17 +14,17 @@ class RedirectedStdStreams:
         # do something that spams stderr
 
     """
-    def __init__(self, stdout=None, stderr=None):
+    def __init__(self, stdout: TextIO = None, stderr: TextIO = None) -> None:
         self._stdout = stdout or sys.stdout
         self._stderr = stderr or sys.stderr
 
-    def __enter__(self):
+    def __enter__(self) -> None:
         self.old_stdout, self.old_stderr = sys.stdout, sys.stderr
         self.old_stdout.flush()
         self.old_stderr.flush()
         sys.stdout, sys.stderr = self._stdout, self._stderr
 
-    def __exit__(self, exc_type, exc_value, traceback):
+    def __exit__(self, t: Optional[Type[BaseException]], e: Optional[BaseException], tb: TracebackType) -> None:
         self._stdout.flush()
         self._stderr.flush()
         sys.stdout = self.old_stdout
@@ -36,14 +40,14 @@ class SilencedStreams(RedirectedStdStreams):
         # do something that spams stderr
 
     """
-    def __init__(self, silence_stdout = True, silence_stderr = True):
+    def __init__(self, silence_stdout: bool = True, silence_stderr: bool = True):
         self.silence_stdout = silence_stdout
         self.silence_stderr = silence_stderr
-        self._stdout = open(os.devnull, 'w') if self.silence_stdout else sys.stderr
+        self._stdout = open(os.devnull, 'w') if self.silence_stdout else sys.stdout
         self._stderr = open(os.devnull, 'w') if self.silence_stderr else sys.stderr
 
-    def __exit__(self, exc_type, exc_value, traceback):
-        super(SilencedStreams, self).__exit__(exc_type, exc_value, traceback)
+    def __exit__(self, t: Optional[Type[BaseException]], e: Optional[BaseException], tb: TracebackType) -> None:
+        super(SilencedStreams, self).__exit__(t, e, tb)
         if self.silence_stdout:
             self._stdout.close()
         if self.silence_stderr:
