4a24668f50eb613cafffcd732b94d409519a9867
Johannes Künsebeck <jkuensebeck@taz.de>
2022-10-23 22:58:30 +0200

    Introduced class FileGroupHandle for FileGroup/MimeType pairs

--- ocrd_browser/util/file_groups.py
+++ ocrd_browser/util/file_groups.py
@@ -1,7 +1,20 @@
 import re
 from collections import Counter
-from typing import List, Tuple, Optional, Counter as CounterType
+from typing import List, Optional, Counter as CounterType, NamedTuple
 
+from ocrd_models import OcrdFile
+
+
+class FileGroupHandle(NamedTuple):
+    group: str
+    mime: str
+
+    @property
+    def key(self) -> str:
+        return '|'.join(self)
+
+    def match(self, file: OcrdFile) -> bool:
+        return file.fileGrp == self.group and file.mimetype == self.mime
 
 
 def weight_match(s: str, preferred: Optional[List[str]] = None) -> float:
@@ -11,25 +24,24 @@ def weight_match(s: str, preferred: Optional[List[str]] = None) -> float:
     weight = 0.0
     if preferred:
         ln = float(len(preferred))
-        for i, preferred in enumerate(preferred):
-            if re.fullmatch(preferred, s):
+        for i, pref in enumerate(preferred):
+            if re.fullmatch(pref, s):
                 # prefer matches earlier in the list
                 weight += (ln - i) / ln
                 # break or no break???
     return weight
 
 
-def best_file_group(file_groups_and_mimetypes: List[Tuple[str, str]], preferred_groups: Optional[List[str]] = None, preferred_mimetypes: Optional[List[str]] = None, cutoff: float = -9999) -> Optional[Tuple[str, str]]:
-    file_groups: CounterType[Tuple[str, str]] = Counter()
-    for file_group_and_mimetype in file_groups_and_mimetypes:
-        file_group, mimetype = file_group_and_mimetype
-        file_groups[file_group_and_mimetype] += weight_match(file_group, preferred_groups)  # type: ignore[assignment]
-        file_groups[file_group_and_mimetype] += weight_match(mimetype, preferred_mimetypes)  # type: ignore[assignment]
-        file_groups[file_group_and_mimetype] -= len(file_group) * 0.0001  # type: ignore[assignment]
-    if file_groups:
-        [(file_group_and_mimetype, score)] = file_groups.most_common(1)
+def best_file_group(file_group_handles: List[FileGroupHandle], preferred_groups: Optional[List[str]] = None, preferred_mimetypes: Optional[List[str]] = None, cutoff: float = -9999) -> Optional[FileGroupHandle]:
+    file_groups_counter: CounterType[FileGroupHandle] = Counter()
+    for file_group_handle in file_group_handles:
+        file_groups_counter[file_group_handle] += weight_match(file_group_handle.group, preferred_groups)  # type: ignore[assignment]
+        file_groups_counter[file_group_handle] += weight_match(file_group_handle.mime, preferred_mimetypes)  # type: ignore[assignment]
+        file_groups_counter[file_group_handle] -= len(file_group_handle.group) * 0.0001  # type: ignore[assignment]
+    if file_groups_counter:
+        [(file_group_handle, score)] = file_groups_counter.most_common(1)
         if score > cutoff:
-            return file_group_and_mimetype
+            return file_group_handle
         else:
             return None
     else:
